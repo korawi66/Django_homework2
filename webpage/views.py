071307a769dd4d63ab0bc64cbcb1b236c47635d9
katawut2004 <mr.katawut2004@gmail.com>
2024-07-02 11:43:44 +0700

    add webpage app

---
+++ webpage/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def indexPage(request):
+    return HttpResponse("Hello, This is my web page")
+
+def aboutUs (request):
+    return HttpResponse("เกี่ยวกับฉัน")
+
+def contactUs (request):
+    return HttpResponse("ติดต่อฉัน")
